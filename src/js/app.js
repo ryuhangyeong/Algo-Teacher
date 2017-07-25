9707dff03016a97e2a24170c770c97f6de86b289
ryuhangyeong <webfrontend@naver.com>
2017-07-25 14:45:37 +0900

    고정 버튼 및 쇼케이스 추가

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,37 @@
+/*
+  모든 자바스크립트와 HTML 번들링 시작점
+
+  프로젝트 이름 : Teacher
+
+  프로젝트 개요
+    자바스크립트와 제이쿼리를 이용한 자료구조 및 알고리즘 시각화 프로젝트
+
+  프로젝트 특징
+    현대적 자바스크립트 웹 개발을 위한 webpack 개발 환경 및 개발 서버 운용
+    모듈 시스템을 적극이용하여 체계적인 개발 코드 작성 및 scss를 이용한 스타일 체계적 작성
+
+  프로젝트 공헌자
+    ryuhangyeong(류한경, 웹 프론트 엔드 개발자)
+    apalsl
+
+  프로젝트 공헌하는 방법
+  프로젝트 Fork 및 자신이 작성 원하는 시각화 알고리즘 및 자료구조 선정
+  app.js 에서 [의존하고 있는 HTML 파일] 에 의존성 추가
+    ex) import '../tree.html'
+    반드시 src 폴더에 HTML 추가할 것
+
+  스타일 파일 작성후 의존성 추가
+    ex) import '../css/plus.scss';
+
+  사용 원하는 서브파티 라이브러리 추가가능
+    ex) lodash 등등
+
+*/
 // 메인 스타일 시트
 import '../css/main.scss';
 
 // 의존하고 있는 HTML 파일들
-import '../stack.html';
+import '../queue.html';
 
 // 의존하고 있는 3rd 라이브러리 및 프레임워크 모음
 import 'jquery';
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,34 +1,6 @@
-// /* 해시값에 의한 스크롤 이동 함수 */
-// function ScrollMoving() {
-//   $('a[href*=\\#]:not([href=\\#])').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-//     && location.hostname == this.hostname) {
-//       var $target = $(this.hash);
-//       $target = $target.length && $target
-//       || $('[name=' + this.hash.slice(1) +']');
-//       if ($target.length) {
-//         var targetOffset = $target.offset().top;
-//         $('html,body')
-//         .animate({scrollTop: targetOffset}, 1000);
-//        return false;
-//       }
-//     }
-//   });
-// }
-//
-// /* 스크롤 높이에 따른 클래스 추가,삭제 함수 */
-// function ScrollHeader() {
-//   $(window).scroll(function() {
-//     if($(document).scrollTop() > 10) {
-//       $('#logo').addClass('shrink');
-//     } else {
-//       $('#logo').removeClass('shrink');
-//     }
-//   })
-// }
-
 var scroll = {
   ScrollMoving: function() {
+    // 해시 값을 인식하여 원하는 id값으로 이동하는 함수
     $('a[href*=\\#]:not([href=\\#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
       && location.hostname == this.hostname) {
@@ -45,6 +17,7 @@ var scroll = {
     });
   },
   ScrollHeader: function() {
+    // 스크롤 높이를 계산하여 특정 높이 이상일 경우 스타일 클래스 추가하는 함수
     $(window).scroll(function() {
       if($(document).scrollTop() > 10) {
         $('#logo').addClass('shrink');
@@ -54,4 +27,5 @@ var scroll = {
     })
   }
 }
+
 export default scroll;
