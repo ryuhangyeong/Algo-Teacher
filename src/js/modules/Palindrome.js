ccc10a23f2bc81fad717c16fc8ee261e38b3e895
ryuhangyeong <webfrontend@naver.com>
2017-08-25 13:40:28 +0900

    회문 검사기 리팩토링(try ~ catch 구문 추가) ;

--- src/js/modules/Palindrome.js
+++ src/js/modules/Palindrome.js
@@ -1,6 +1,9 @@
-import Stack from './Stack';
 import palindromeData from '../data/palindromeData';
+import Stack from './Stack';
+import Error from '../utils/Error'; // 에러 핸들링
+import Alert from '../utils/Alert'; // Materialize 알림창
 
+// 리팩토링(17/8/24)
 var Palindrome = {
   palindrome_create: $('#palindrome_create'),
   palindrome_input: $('#palindrome_input'),
@@ -16,20 +19,25 @@ var Palindrome = {
     this.palindrome_create.click(() => {
       var text = this.palindrome_input.val();
 
-      if(text == "") {
-        Materialize.toast('입력폼이 비어있습니다!', 2000, 'rounded')
-        return;
+      try {
+        if(text == "") {
+          throw new Error('NOT INPUT', '입력폼이 비어있습니다!');
+        }
+      } catch(e) {
+        if(e.name == 'NOT INPUT') {
+          Alert(e.message, 3000);
+          return;
+        }
       }
       this.isPalindrome(text)
     });
-
     this.palindrome_remove.click(() => {
       this.inputvalue();
     })
   },
 
   isPalindrome(word) {
-    word = word.replace( /(\s*)/g, "");
+    word = word.replace( /(\s*)/g, ""); // 모든 공백 제거
     for(var i = 0; i < word.length; i++) {
       this.stack.push(word[i]);
     }
@@ -40,15 +48,14 @@ var Palindrome = {
       rword += this.stack.pop();
     }
     if(word == rword) {
-      Materialize.toast('회문입니다!', 2000, 'rounded')
+      Alert('회문입니다!', 2000);
     } else {
-      Materialize.toast('회문이 아닙니다!', 2000, 'rounded')
+      Alert('회문이 아닙니다!', 2000);
     }
   },
 
   inputvalue: function() {
     var data = palindromeData();
-
     this.palindrome_input.val(data);
   }
 }
